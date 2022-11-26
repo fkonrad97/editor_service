4e99f41f548c0643248a2b276be2111b690fc37d
fkonrad97 <fkonrad97@gmail.com>
2022-11-26 22:05:51 +0100

    error handling with middleware and express-async-errors

--- api/startup/routes.js
+++ api/startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const stories = require('../routes/stories');
 const deployedStories = require('../routes/deployedstories');
+const error = require('../middleware/error');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/api/stories', stories);
     app.use('/api/deployedstories', deployedStories);
+    app.use(error);
 }
