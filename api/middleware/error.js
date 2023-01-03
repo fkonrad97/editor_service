3e660d964f041ce5c16722247515a91f99cb2675
fkonrad97 <fkonrad97@gmail.com>
2023-01-03 18:15:53 +0100

    updates

--- api/middleware/error.js
+++ api/middleware/error.js
@@ -2,7 +2,12 @@ const winston = require('winston');
 
 module.exports = function(err, req, res, next){
     // Log the exception
-    winston.error(err.message, err);
+    winston.error(err);
 
-    res.status(500).send(err.message);
+    // Send a user-friendly message to the client
+    res.status(500).send({
+        message: 'An error occurred while processing your request. Please try again later.',
+        errorCode: err.code
+        // Add any other relevant information about the error here
+    });
 }
