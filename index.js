861ed3d26755d1aa933e3a7f6e7a0633046cfc70
fkonrad97 <fkonrad97@gmail.com>
2023-03-12 18:57:15 +0100

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('./api/startup/logging')();
-require('./api/startup/routes')(app);
-require('./api/startup/database')();
+require('./editor/startup/logging')();
+require('./editor/startup/routes')(app);
+require('./editor/startup/database')();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
